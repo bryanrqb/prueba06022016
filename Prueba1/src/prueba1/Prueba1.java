395a6b3c31330bf40aa051bce6ab1fdca98223ed
PC <PC@PC-PC>
2017-02-06 11:55:25 -0500

    1er commit

--- Prueba1/src/prueba1/Prueba1.java
+++ Prueba1/src/prueba1/Prueba1.java
@@ -7,7 +7,7 @@ public class Prueba1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) {//Mediante este método java sabe por dónde va a empezar a compilar el programa
         String mate;
         String fisica;
         String quimica;
